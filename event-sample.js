5728a7ed7be0b467259d6a25eb0fa32990904619
Martin Volerich <martin@volerich.com>
2016-10-29 14:12:13 -0700

    Corrected bugs

---
+++ event-sample.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "session": {
+        "application": {
+            "applicationId": "TEST"
+        },
+        "user": {
+            "userId": "Anonymous"
+        }
+    },
+    "request": {
+        "type": "IntentRequest",
+        "intent": {
+            "name": "GetTideIntent",
+            "slots": {
+                "City": {
+                    "name": "City",
+                    "value": "Boston"
+                }
+            }
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
 var Alexa = require('alexa-sdk');
 
-require("dotenv").config();
+require('dotenv').config();
 
-var SKILL_NAME = "Tide Pooler";
+var SKILL_NAME = 'Tide Pooler';
 
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, callback);
-    alexa.APP_ID = APP_ID
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = process.env.APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
@@ -22,16 +22,30 @@ var handlers = {
         var cityName;
         if (citySlot && citySlot.value) {
             cityName = citySlot.value;
+
             var cardTitle = SKILL_NAME + " High Tide For - " + cityName;
             var time = "5:00pm";
-            var speechOutput = "I'm sorry I don't know when high tide is for that location";
+            var speechOutput = "It will be high tide in " +
+                cityName + " at " + time;
+            this.emit(':tellWithCard', speechOutput, SKILL_NAME,
+                cardTitle, time);
+
+        } else {
+            var speechOutput =
+                'I\'m sorry, I don\'t know when high tide is for that location';
             this.emit(':tell', speechOutput);
         }
     },
-    'AMAZON.HelpIntent': function() {
-         var speechOutput = "You can say when is high tide in city name, or, " +
-             "you can say exit... What can I help you with?";
-         var reprompt = "What can I help you with?";
-         this.emit(':ask', speechOutput, reprompt)
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = "You can say when is high tide in city name, or, " +
+            "you can say exit... What can I help you with?";
+        var reprompt = "What can I help you with?";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
     }
-}
+};
